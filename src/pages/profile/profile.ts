f278f937f876fafa1b7bbd34d9a65dc23cb17f53
mob35 <toonelicious@gmail.com>
2018-01-19 10:24:40 +0700

    update

--- src/pages/profile/profile.ts
+++ src/pages/profile/profile.ts
@@ -51,40 +51,44 @@ export class ProfilePage {
       this.user.gender = this.user.gender.toUpperCase();
     }
     this.makeProfile = [{
-      rank: 1,
-      name: this.user.displayName,
-      image: this.user.profileImageURL,
-      coin: 50000
+      rank: 5031,
+      image: 'https://upload.wikimedia.org/wikipedia/commons/f/f4/Sunny_at_Incheon_Airport_on_May_20%2C_2015.jpg',
     },
     {
-      rank: 2,
-      name: this.user.displayName,
+      rank: 5032,
       image: this.user.profileImageURL,
-      coin: 50000
     },
     {
-      rank: 3,
-      name: this.user.displayName,
-      image: this.user.profileImageURL,
-      coin: 50000
+      rank: 5033,
+      image: 'https://i.ytimg.com/vi/Cz1EcFnoNmU/maxresdefault.jpg',
     },
     {
-      rank: 4,
-      name: this.user.displayName,
-      image: this.user.profileImageURL,
-      coin: 50000
+      rank: 5034,
+      image: 'http://tv.bectero.com/wp-content/uploads/2017/07/big_147202988557bd64bdb8560.jpg',
     },
     {
-      rank: 5,
-      name: this.user.displayName,
-      image: this.user.profileImageURL,
-      coin: 50000
+      rank: 5035,
+      image: 'http://kpop.youzab.com/wp-content/uploads/2017/10/Girls-Generation-YoonA.jpg',
     },
     {
-      rank: 6,
-      name: this.user.displayName,
-      image: this.user.profileImageURL,
-      coin: 50000
+      rank: 5036,
+      image: 'https://1.soompi.io/wp-content/blogs.dir/12/files/2013/11/girls-generation-seohyun-wide.jpg',
+    },
+    {
+      rank: 5037,
+      image: 'https://sites.google.com/site/soshichotima/_/rsrc/1468857530546/-suny/-suny/-hyoyeon/922a6f268d22132177ab39f9d304555e.jpg?height=300&width=400',
+    },
+    {
+      rank: 5038,
+      image: 'https://image.dek-d.com/27/0456/5187/120458333',
+    },
+    {
+      rank: 5039,
+      image: 'https://1.bp.blogspot.com/-cyY0fYSWpys/VwYDtSPoAvI/AAAAAAAAAFg/Wop9RnoATmUhO81_FznYMkHtoqZ-r-VbA/s1600/7ce499706e4c7712d5a64ba46c453dfc.jpg',
+    },
+    {
+      rank: 5040,
+      image: 'http://topicstock.pantip.com/chalermkrung/topicstock/2011/03/C10370545/C10370545-7.jpg',
     }];
   }
 
