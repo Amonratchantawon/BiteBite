ddb87b2ae995f8c7ed9025c819c0ee41710f6f3d
secretmoo35 <sarayut-moo.th@hotmail.com>
2018-01-22 14:40:48 +0700

    Update: order

--- src/pages/order/order.ts
+++ src/pages/order/order.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { CartProvider } from '../../providers/cart/cart';
 import { CartModel } from '../../assets/model/cart.model';
+import { UserModel } from '../../assets/model/user.model';
+import { Constants } from '../../app/app.constants';
 
 /**
  * Generated class for the OrderPage page.
@@ -17,6 +19,7 @@ import { CartModel } from '../../assets/model/cart.model';
 })
 export class OrderPage {
   order: CartModel = new CartModel();
+  user: UserModel = new UserModel();
   constructor(
     public navCtrl: NavController,
     public navParams: NavParams,
@@ -26,6 +29,7 @@ export class OrderPage {
 
   ionViewWillEnter() {
     this.order = this.cartProvider.getCartByShop();
+    this.user = this.user = JSON.parse(window.localStorage.getItem('user@' + Constants.URL));
   }
 
 }
