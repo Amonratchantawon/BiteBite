af4213dabe7f5da68b5abdde0d4663bc8668d9a6
amonratCha <amonrat.cha@northbkk.ac.th>
2018-01-25 09:51:06 +0700

    history detail

--- src/pages/history-detail/history-detail.module.ts
+++ src/pages/history-detail/history-detail.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
 import { HistoryDetailPage } from './history-detail';
+import { TranslateModule } from '@ngx-translate/core';
 
 @NgModule({
   declarations: [
@@ -8,6 +9,7 @@ import { HistoryDetailPage } from './history-detail';
   ],
   imports: [
     IonicPageModule.forChild(HistoryDetailPage),
+    TranslateModule.forChild()
   ],
 })
 export class HistoryDetailPageModule {}
