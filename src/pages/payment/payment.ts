aa585d2658ed67d1624510fdd1e98f40c9f8fd82
amonratCha <amonrat.cha@northbkk.ac.th>
2018-01-25 12:18:08 +0700

    playment alert loading translet

--- src/pages/payment/payment.ts
+++ src/pages/payment/payment.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, AlertController } from 'ionic-angular';
+import { LoadingProvider } from '../../providers/loading/loading';
+import { AlertProvider } from '../../providers/alert/alert';
+import { TranslateService } from '@ngx-translate/core';
 
 /**
  * Generated class for the PaymentPage page.
@@ -15,15 +18,55 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class PaymentPage {
   payment: string = '1';
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(
+    public navCtrl: NavController,
+    public navParams: NavParams,
+    private loading: LoadingProvider,
+    public alert: AlertProvider,
+    private translate: TranslateService,
+    private alertCtrl: AlertController
+  ) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad PaymentPage');
   }
 
-  onPayment(){
-    this.navCtrl.push('NavtabsPage');
+  onPayment() {
+    let language = this.translate.currentLang;
+    let title = '';
+    let massage = '';
+    let button = '';
+
+    this.loading.onLoading();
+    setTimeout(() => {
+      this.loading.dismiss();
+
+      if (language == 'th') {
+        title = 'รายการสั่งซื้อสำเร็จ';
+        massage = 'ขอบคุณที่ใช้บริการ';
+        button = 'ตกลง';
+      } else if (language == 'en') {
+        title = 'Order successfully';
+        massage = 'Thank you';
+        button = 'OK';
+      }
+      
+      let alert = this.alertCtrl.create({
+        title: title,
+        message: massage,
+        buttons: [
+          {
+            text: button,
+            handler: () => {
+              this.navCtrl.push('NavtabsPage');
+            }
+          }
+        ]
+      });
+      alert.present();
+    }, 3000);
+
   }
 
 }
