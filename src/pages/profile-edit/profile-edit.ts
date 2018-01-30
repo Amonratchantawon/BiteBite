4e5baf6cca8b10413038362e8a9f8a376ea86c35
secretmoo35 <sarayut-moo.th@hotmail.com>
2018-01-30 16:21:15 +0700

    update

--- src/pages/profile-edit/profile-edit.ts
+++ src/pages/profile-edit/profile-edit.ts
@@ -30,7 +30,7 @@ export class ProfileEditPage {
 
   updateProfile() {
     this.auth.updateUser(this.user).then((res) => {
-      this.navCtrl.push('ProfilePage');
+      this.navCtrl.pop();
     });
 
   }
