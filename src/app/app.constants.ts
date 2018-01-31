a5667d90d1671cb6c0ce0a7063c8cb41f50688c7
secretmoo35 <sarayut-moo.th@hotmail.com>
2018-01-31 09:47:42 +0700

    Update: color

--- src/app/app.constants.ts
+++ src/app/app.constants.ts
@@ -1,5 +1,6 @@
 export class Constants {
-    public static get URL(): string { return "https://eatsyd-test.herokuapp.com"; };
+    // public static get URL(): string { return "https://eatsyd-test.herokuapp.com"; };
+    public static get URL(): string { return "https://vintage-server.herokuapp.com"; };    
     public static get OmiseKey(): any {
         let key = {
             'publicKey': 'pkey_test_5asc1ucstk1imcxnhy7',
